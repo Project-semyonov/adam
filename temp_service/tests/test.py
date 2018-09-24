0f59f0e90cf8231c356c312edbd4f52006510595
joe <joseph0brennan@gmail.com>
2018-09-24 10:31:32 -0600

    i will figure it out

--- temp_service/tests/test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from ..source import example
-class Test_example(unittst.TestCase):
-    def test_one(self):
-        self.assertEqual(example.mirror(1), 1)
-
-if __name__=='__main__':
-    unittest.main()

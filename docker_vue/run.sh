9ca6c494d18a0ce9c72a631d7518af0cb2e39f09
Joey <sharpshooter30@gmail.com>
2018-10-13 14:01:34 -0600

    spelling is hard

--- docker_vue/run.sh
+++ docker_vue/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t semyonove4360/vue_cli .
-docker run -it -p 8080:8080 --rm --name vue-cli semyonove4360/vue_cli
+docker build -t semyonov4360/vue_cli .
+docker run -it -p 8080:8080 --rm --name vue-cli semyonov4360/vue_cli

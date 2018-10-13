a9e34c2131101488663ad1584878cb4e7087a415
eli-skaronea <eskaronea@gmail.com>
2018-10-13 12:40:04 -0600

    Started development on Vue SPA

---
+++ vue_app/client/src/components/TempSensor.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul id="Temps">
+  <li v-for="temp in tempData">
+    {{ tempData }}
+  </li>
+</ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      tempData: '',
+    };
+  },
+  methods: {
+    getTemps() {
+      const path = 'http://localhost:5000/temp/all';
+      axios.get(path)
+        .then((res) => {
+          this.tempData = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getTemps();
+  },
+};
+
+</script>
+

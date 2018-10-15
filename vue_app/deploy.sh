3734eb868546fb7838f5c379de78df2f27f99393
Joey <sharpshooter30@gmail.com>
2018-10-15 15:47:42 -0600

    fixed the CF being convered to the CRLF and changed the LF in the needed files

--- vue_app/deploy.sh
+++ vue_app/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker build -t semyonov4360/vue_app:rpi . 
+docker build -t semyonov4360/vue_app:rpi .
 docker login -u semyonov4360 --password-stdin
 docker push semyonov4360/vue_app:rpi
-

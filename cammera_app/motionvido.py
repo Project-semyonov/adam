f9ddbdfffbc90be8aadd532d4422319adddd75bd
Joey <sharpshooter30@gmail.com>
2018-10-17 14:48:59 -0600

    fixed the python video file to actual human readablity

--- cammera_app/motionvido.py
+++ cammera_app/motionvido.py
@@ -7,48 +7,96 @@ import time
 from datetime import datetime
 from PIL import Image
 
-camera = picamera.PiCamera()
 
-difference = 20
-pixels = 100
+class MotionVidoe:
+    def __init__(self):
+        self.camera = picamera.PiCamera()
 
-width = 1280
-height = 960
+        self.rec_length = 30
 
-def compare():
-	camera.resolution = (640, 480)
-	stream = io.BytesIO()
-	camera.capture(stream, format = 'bmp')
-	stream.seek(0)
-	im = Image.open(stream)
-	buffer =  im.load()
-	stream.close()
-	return im, buffer
+        self.difference = 20
 
-def newvideo(width, height):
-	time = datetime.now()
-	filename = 'motionvideo-%04d%02d%02d-%02d%02d%02d.h264' %(time.year,time.month,time.day, time.hour,time.minute,time.second)
-	camera.resolution = (width, height)
-	camera.start_recording(filename)
-	camera.wait_recording(30)
-	camera.stop_recording()
-	print 'Captured %s' % filename
-image1, buffer1 = compare()
+        self.pixels = 100
 
-timestamp = time.time()
+        self.width = 1280
+        self.height = 960
 
-while(True):
-	image2, buffer2 = compare()
-	
-	changedpixels = 0
-	for x in range(0, 100):
-		for y in xrange(0, 75):
-			pixdiff =  abs(buffer1[x,y][1]-buffer2[x,y][1])
-			if pixdiff > difference:
-				changedpixels += 1
-	if changedpixels > pixels:
-		timestamp =  time.time()
-		newvideo(width, height)
-	image1 = image2
-	buffer1 = buffer2	  
- 
+    def compare(self):
+        self.camera.resolution = (640, 480)
+
+        stream = io.BytesIO()
+
+        self.camera.capture(stream, format='bmp')
+
+        stream.seek(0)
+
+        im = Image.open(stream)
+
+        buffer = im.load()
+
+        stream.close()
+
+        return im, buffer
+
+    def new_video(self):
+        time = datetime.now()
+
+        filename = 'motion-video-%04d%02d%02d-%02d%02d%02d.h264' % (
+            time.year, time.month, time.day, time.hour, time.minute, time.second)
+
+        self.camera.resolution = (self.width, self.height)
+
+        self.camera.start_recording(filename)
+
+        self.camera.wait_recording(self.rec_length)
+
+        self.camera.stop_recording()
+
+        print('Captured %s' % filename)
+
+    # image1, buffer1 = compare()
+
+    # timestamp = time.time()
+
+
+if __name__ == '__main__':
+    cam = MotionVidoe()
+
+    image1, buffer1 = cam.compare()
+    
+    while not cam.new_video():
+        image2, buffer2 = cam.compare()
+
+        change_pixels = 0
+
+        for x in range(0, 100):
+            for y in range(0, 100):
+                pix_diff = abs(buffer1[x, y][1] - buffer2[x, y][1])
+
+                if pix_diff > cam.difference:
+                    change_pixels += 1
+
+                if change_pixels > cam.pixels:
+                    timestamp = time.time()
+
+                    cam.new_video()
+
+                image1 = image2
+
+                buffer1 = buffer2
+"""
+    while True:
+        image2, buffer2 = cam.compare()
+
+        changedpixels = 0
+        for x in range(0, 100):
+            for y in xrange(0, 75):
+                pixdiff = abs(buffer1[x, y][1] - buffer2[x, y][1])
+                if pixdiff > difference:
+                    changedpixels += 1
+        if changedpixels > pixels:
+            timestamp = time.time()
+            newvideo(width, height)
+        image1 = image2
+        buffer1 = buffer2
+"""

3a1df8955763f0b11cc42a6bd4e6abd664f8a1a2
joe <joseph0brennan@gmail.com>
2018-10-16 18:18:45 -0600

    automating the function to look for the password.txt file in the adam directory

--- vue_app/deploy.sh
+++ vue_app/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-docker build -t semyonov4360/vue_app:rpi .
-docker login -u semyonov4360 --password-stdin
-docker push semyonov4360/vue_app:rpi
+#docker build -t semyonov4360/vue_app:rpi .
+if [ -e ../password.txt ]
+then 
+    myVar=$(cat ../password.txt)
+    
+    docker login -u semyonov4360 -p "$myVar"
+    
+    docker push semyonov4360/vue_app:rpi
+
+else
+    docker login -u semyonov4360 --password-stdin
+    
+    docker push semyonov4360/vue_app:rpi
+fi

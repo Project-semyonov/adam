d94e8451c906361adf4df8eeb251940064d0b82a
joe <joseph0brennan@gmail.com>
2018-10-16 18:33:27 -0600

    updated the other three docker deployments

--- flask_service/builder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t semyonov4360/flask_endpoint:rpi . 
-docker login -u semyonov4360
-docker push semyonov4360/flask_endpoint:rpi
--- temp_service/builder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t semyonov4360/test_python:rpi . 
-docker login -u semyonov4360
-docker push semyonov4360/test_python:rpi

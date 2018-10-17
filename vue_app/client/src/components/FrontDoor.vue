e0e23daacbd30f7803498f0cef680aecf40d8dfe
eli-skaronea <eskaronea@gmail.com>
2018-10-17 17:05:23 -0600

    modified header buttons

--- vue_app/client/src/components/FrontDoor.vue
+++ vue_app/client/src/components/FrontDoor.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="homepage-container">
     <div class="header">
-      <router-link to="/home" tag="button" class="btn btn-outline-warning">Homepage</router-link>
-      <router-link to="/door/front" tag="button" class="btn btn-warning">Front Door</router-link>
+      <div class="home-button">
+        <router-link to="/home" tag="button" class="btn btn-outline-danger">Homepage</router-link>
+      </div>
+      <router-link to="/door/front" tag="button" class="btn btn-warning"
+        >Front Door</router-link>
       <router-link to="/temp/all" tag="button" class="btn btn-outline-warning"
         >Temp Sensor</router-link>
-      <a href="https://github.com/Project-semyonov/adam" tag="button"
+      <div class="github-button">
+        <a href="https://github.com/Project-semyonov/adam" tag="button"
         class="btn btn-outline-success">Github Repo</a>
+      </div>
       <h1>Front Door Camera</h1>
     </div>
       <p> WHO IS STEALING MY PACKAGES?! </p>

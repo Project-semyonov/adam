58d881a9eccbb53c5a646854ee59eae81f27599c
Joey <sharpshooter30@gmail.com>
2018-10-22 13:01:55 -0600

    trying to improve the script

--- camera_app/run.sh
+++ camera_app/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker build -t semyonov/camera_app .
-docker run --rm -d --privileged semyonov/camera_app
+if docker container ls -a | grep "camera_app"
+        docker restart camera_app
+else
+        docker build -t semyonov/camera_app . 
+        docker run --rm -d --privileged --name camera_app semyonov/camera_app
+fi

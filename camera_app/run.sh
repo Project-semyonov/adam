9dc1d3ea9c34633cdbe82b14468acfeff6202d82
Joseph-Brennan <joseph0brennan@gmail.com>
2018-10-24 21:18:49 -0600

    fixing my scripts and possibly using the tests once they work

--- camera_app/run.sh
+++ camera_app/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if docker container ls -a | grep "camera_app"
+if [-e docker container ls -a | grep "camera_app"]
+then
         docker restart camera_app
 else
-        docker build -t semyonov/camera_app . 
-        docker run --rm -d --privileged --name camera_app semyonov/camera_app
+        docker build -t semyonov4360/camera_app . 
+        docker run -d --privileged --name camera_app semyonov4360/camera_app
 fi
--- vue_app/run.sh
+++ vue_app/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build -t semyonov4360/vue_app .
-docker run -it -p 8080:8080 --rm --name vue-app semyonov4360/vue_app
+if [-e docker container ls -a | grep "vue_app"]
+then
+    docker reastart vue_app
+
+else
+    docker build -t semyonov4360/vue_app .
+    
+    docker run -it -p 8080:8080 --name vue-app semyonov4360/vue_app
+fi

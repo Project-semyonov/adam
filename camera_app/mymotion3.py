c6be241f5339fa24621d994cd1f7a4ae0c7dac0f
Joey <sharpshooter30@gmail.com>
2018-10-27 12:23:13 -0600

    small edits for python

--- camera_app/mymotion3.py
+++ camera_app/mymotion3.py
@@ -3,14 +3,12 @@ import picamera
 from PIL import Image
 from PIL import imageChops
 
+
 class camera():
-    self.camera = picamera.Picamera()
-    self.stream = picamera.PicameraCircularIO(camera, seconds = 20)
-    
+    def method(self):
+        self.camera = picamera.Picamera()
+        self.stream = picamera.PicameraCircularIO(camera, seconds=20)
 
     def motion(self):
         self.camera.resolution = (640, 480)
         self.camera.framerate = 32
-        
-
-

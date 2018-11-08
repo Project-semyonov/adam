8387fd737b0730bb1b7b4019adc05e6c916ffa25
joe <joseph0brennan@gmail.com>
2018-11-08 16:51:08 -0700

    the dockerfile and the script work and are good to go

--- camera_app/run.sh
+++ camera_app/run.sh
@@ -3,7 +3,6 @@ if [ "$(docker container ls -a | grep -c "camera_app")" -ge 1 ]
 then
         docker restart camera_app
 else
-        docker build -t semyonov4360/camera_app:rpi . 
-        docker run -d --privileged -it --name camera_app semyonov4360/camera_app:rpi
+        docker build -t semyonov4360/camera_app:rpi .
         docker run -d -v /home/pi/Documents/adam/camera_app/videos/:/root/videos/ --privileged -it --name camera_app semyonov4360/camera_app:rpi
 fi

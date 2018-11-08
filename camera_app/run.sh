13b6930726efaf678f39bd453e8bcbb2d1adfd3c
joe <joseph0brennan@gmail.com>
2018-11-08 16:10:08 -0700

    backup of my script

--- camera_app/run.sh
+++ camera_app/run.sh
@@ -5,4 +5,5 @@ then
 else
         docker build -t semyonov4360/camera_app:rpi . 
         docker run -d --privileged -it --name camera_app semyonov4360/camera_app:rpi
+        docker run -d -v /home/pi/Documents/adam/camera_app/videos/:/root/videos/ --privileged -it --name camera_app semyonov4360/camera_app:rpi
 fi

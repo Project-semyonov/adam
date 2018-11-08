a343b8c30b3d83765b1fde94b34ef7a48aace69b
joe <joseph0brennan@gmail.com>
2018-11-07 17:47:56 -0700

    updated the run script to properly use the rpi version and the dockerfile needs to be reworked so this is my first attempt for role back

--- camera_app/run.sh
+++ camera_app/run.sh
@@ -3,6 +3,6 @@ if [ "$(docker container ls -a | grep -c "camera_app")" -ge 1 ]
 then
         docker restart camera_app
 else
-        docker build -t semyonov4360/camera_app . 
-        docker run -d --privileged --name camera_app semyonov4360/camera_app
+        docker build -t semyonov4360/camera_app:rpi . 
+        docker run -d --privileged --name camera_app semyonov4360/camera_app:rpi
 fi

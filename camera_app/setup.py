8323d801480f5bbcfe9c67ae9fa130931b4d9b79
joe <joseph0brennan@gmail.com>
2018-11-12 16:29:11 -0700

    using the link roach gave me i updated the docker file and the setup to use the minimal package

---
+++ camera_app/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='camera',
+      version='0.4',
+      url='github.com/Project-semyonov/adam',
+      author='Semyonov Team',
+      author_email='jbrenn11@msudenver.edu',
+      license='MIT',
+      packages=['camera_app'],
+      zip_safe=False)

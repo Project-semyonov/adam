7c21ea40b77e40a303a86eab2f389adace812e98
Joey <sharpshooter30@gmail.com>
2018-12-01 12:31:46 -0700

    this should be ready for deploy now going to look into aws-cli

--- push_camera/converter.sh
+++ push_camera/converter.sh
@@ -2,10 +2,10 @@
 
 for f in ~/Videos/*.h264;
 do
-if [ ! -f "${f%.h264}.mp4" ] 
+if [ ! -f "${f%.h264}.mp4" ]
 then
 #    echo $f
 #    echo ${f%.h264}.mp4
-    MP4Box -add $f ${f%.h264}.mp4
+    ffmpeg -i $f -c copy ${f%.h264}.mp4
 fi
 done

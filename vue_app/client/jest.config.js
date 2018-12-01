58852ae5daf60edf7b418057b6fdfb9ac2099171
FredGruetzner <fgruetzn@msudenver.edu>
2018-12-01 14:43:42 -0700

    added mp4 file option

--- vue_app/client/jest.config.js
+++ vue_app/client/jest.config.js
@@ -11,12 +11,13 @@ module.exports = {
 ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2|mp4)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
+  
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
